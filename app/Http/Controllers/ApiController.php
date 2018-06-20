e43dbe7e2756df16e4015259ca46b8c71cf7f1a1
ZacharyToney <zach@clearseries.com>
2018-06-20 14:37:27 -0400

    Tests write to ExampleTest.php, adjusted text output for false input

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -47,7 +47,7 @@ class ApiController extends Controller
         if ($this->checkIfInputGiven($email) === false) {
             return Response::json([
                 "success" => false,
-                "message" => 'No input given.'
+                "message" => 'No valid input given.'
             ]);
         }
 
@@ -59,8 +59,15 @@ class ApiController extends Controller
             ]);
         }
 
+        $inDatabase = User::where('email',$email) -> first();
 
-        //
+        if (empty($inDatabase)) {
+            return Response::json([
+                "success" => false,
+                "message" => 'Email given not in database.'
+            ]);
+        }
+
         $contact = $this->checkIfExistsInInfusionsoft($email);
         //Check if email exists in infusionsoft
         if ($contact === false) {
@@ -79,12 +86,13 @@ class ApiController extends Controller
             $tagId = $this->getTagId($email, $contactProducts);
 
             $infusionsoft = new InfusionsoftHelper();
+
             $infusionsoft->addTag($contactId, $tagId);
+
             return Response::json([
                 "success" => true,
                 "message" => "Contact: " . $contactId . " Has had the tag: " . $tagId ." attached to their account."
             ]);
-
         }
 
     }
